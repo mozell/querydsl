2ec62c9349a5b93ec30b34e506d30ceaf6b16095
mozell <mozellatto@gmail.com>
2022-08-15 16:15:33 +0900

    중급 문법, 프로젝션과 결과 반환 - DTO 조회 (2. Querydsl 빈 생성(Bean population))

--- src/main/java/study/querydsl/dto/UserDto.java
+++ src/main/java/study/querydsl/dto/UserDto.java
@@ -6,13 +6,13 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
-public class MemberDto {
+public class UserDto {
 
-    private String username;
+    private String name;
     private int age;
 
-    public MemberDto(String username, int age) {
-        this.username = username;
+    public UserDto(String name, int age) {
+        this.name = name;
         this.age = age;
     }
 }
