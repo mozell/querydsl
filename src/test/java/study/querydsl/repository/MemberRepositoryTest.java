57747c6f9a1b8d136b1c66e9ab264e54d7ca3eee
mozell <mozellatto@gmail.com>
2022-08-18 21:15:06 +0900

    스프링 데이터 JPA가 제공하는 Querydsl 기능, 인터페이스 지원 - QuerydslPredicateExecutor

--- src/test/java/study/querydsl/repository/MemberRepositoryTest.java
+++ src/test/java/study/querydsl/repository/MemberRepositoryTest.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberSearchCondition;
 import study.querydsl.dto.MemberTeamDto;
 import study.querydsl.entity.Member;
+import study.querydsl.entity.QMember;
 import study.querydsl.entity.Team;
 
 import javax.persistence.EntityManager;
@@ -94,4 +95,20 @@ class MemberRepositoryTest {
         assertThat(result.getSize()).isEqualTo(3);
         assertThat(result.getContent()).extracting("username").containsExactly("member1", "member2", "member3");
     }
+    
+    @Test
+    public void querydslPredicateExecutorTest() throws Exception {
+        Team teamA = new Team("teamA");
+        em.persist(teamA);
+        Member member1 = new Member("member1", 10, teamA);
+        em.persist(member1);
+
+        QMember member = QMember.member;
+
+        Iterable<Member> result = memberRepository.findAll(member.age.between(10, 40).and(member.username.eq("member1")));
+        for (Member findMember : result) {
+            System.out.println("findMember = " + findMember);
+        }
+    }
+    
 }
